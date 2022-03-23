5bef7147c78b4b583ba7aa7bbbf74292eb54df18
dmanibarnett <dmani.barnett@gmail.com>
2022-03-23 14:14:04 +0000

    Configured the routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  VStack,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react';
+import Home from './routes/Home';
+import Services from './routes/Services';
+import About from './routes/About';
+import Contact from './routes/Contact';
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <VStack spacing={8}>
-          </VStack>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path='about' element={<About />} />
+              <Route path='services' element={<Services />} />
+              <Route path='contact' element={<Contact />} />
+            </Routes>
+          </BrowserRouter>
         </Grid>
       </Box>
     </ChakraProvider>
