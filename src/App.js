8567080e37666d44a39b364b8d3475b9ce5aef0a
dmanibarnett <dmani.barnett@gmail.com>
2022-03-23 15:30:43 +0000

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react';
+import { ChakraProvider, Box,theme } from '@chakra-ui/react';
 import Home from './routes/Home';
 import Services from './routes/Services';
 import About from './routes/About';
 import Contact from './routes/Contact';
+import NavBar from './components/NavBar';
+
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <BrowserRouter>
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path='about' element={<About />} />
-              <Route path='services' element={<Services />} />
-              <Route path='contact' element={<Contact />} />
-            </Routes>
-          </BrowserRouter>
-        </Grid>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route index path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="services" element={<Services />} />
+            <Route path="contact" element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
       </Box>
     </ChakraProvider>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import { HStack, Link,Spacer} from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+const NavBar = () => {
+  return (
+    <HStack spacing={3} height='auto'>
+      <Spacer />
+      <Link as={RouterLink} to="/home">
+        Home
+      </Link>
+      <Link as={RouterLink} to="/about">
+        About
+      </Link>
+      <Link as={RouterLink} to="/services">
+        Services
+      </Link>
+      <Link as={RouterLink} to="/contact">
+        Contact
+      </Link>
+    </HStack>
+  );
+};
+
+export default NavBar;
